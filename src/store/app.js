cea490337e2119a8d2c15ab08ad92305d430b567
Q300Z <34000576+Q300Z@users.noreply.github.com>
2023-06-16 00:47:58 +0200

    Update app.js

--- src/store/app.js
+++ src/store/app.js
@@ -24,7 +24,7 @@ import api from "@/api/factures.js";
 
 export const useAppStore = defineStore("app", {
   state: () => ({
-    API_URL: "http://192.168.1.103:3001",
+    API_URL: "https://api.qalpuch.cc",
     compta: [],
     categorie: [],
     editedIndex: -1,
